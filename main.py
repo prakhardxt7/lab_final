696a0df33a6207421638e24fe4306b936742bb53
Prakhar Dixit <54679515+prakhardxt7@users.noreply.github.com>
2024-12-08 19:42:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class PredictRequest(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the FastAPI Simple Linear Regression API and I made some changes."}
+    return {"message": "gbgbgbgfgWelcome to the FastAPI Simple Linear Regression API and I made some changes."}
 
 @app.post("/predict/")
 def predict(request: PredictRequest):
